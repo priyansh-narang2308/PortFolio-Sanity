cb20a48ab9d167f3d231a7e5b0377ef56f895852
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-09 21:10:56 +0530

    feat: Skill shchema added

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import type { SchemaTypeDefinition } from "sanity";
 import profile from "./profile";
 import project from "./project";
+import skill from "./skill";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, project],
+  types: [profile, project,skill],
 };

70c091e3a8114facd85fdf1471d6917e4415c2c8
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-10 14:53:59 +0530

    feat: Experience schema implemented + Index.ts added

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,7 +2,8 @@ import type { SchemaTypeDefinition } from "sanity";
 import profile from "./profile";
 import project from "./project";
 import skill from "./skill";
+import experience from "./experience";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, project,skill],
+  types: [profile, project, skill, experience],
 };

62e8fd2b7054e92cf89113087abe6c424505dde0
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-10 14:56:37 +0530

    Education schema added + added in Index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -3,7 +3,8 @@ import profile from "./profile";
 import project from "./project";
 import skill from "./skill";
 import experience from "./experience";
+import education from "./education";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, project, skill, experience],
+  types: [profile, project, skill, experience, education],
 };

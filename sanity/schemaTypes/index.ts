3aa075b93f53d59dd24e667f274520c49e4e01c0
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-10 19:49:15 +0530

    feat: Certification Schema implemented + Added in the index main file

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,7 +4,8 @@ import project from "./project";
 import skill from "./skill";
 import experience from "./experience";
 import education from "./education";
+import certificstion from "./certificstion";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [profile, project, skill, experience, education],
+  types: [profile, project, skill, experience, education, certificstion],
 };

5d8ef0e4205ae616607952efad7c97b1eb68ee9d
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-14 18:32:23 +0530

    feat: Shadcn Initilizled and deleted all the mock data available

--- app/(portfolio)/page.tsx
+++ app/(portfolio)/page.tsx
@@ -1,5 +1,9 @@
-import { SignInButton } from "@clerk/nextjs";
+
 
 export default function Home() {
-  return <div className="text-5xl">Hello  hOWRE YOU <SignInButton mode="modal"/> </div>;
+  return (
+    <main className="min-h-screen">
+
+    </main>
+  )
 }

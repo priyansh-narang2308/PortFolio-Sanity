89850a47ac2fc36991dc962d05d1b904baeea4ff
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-29 16:52:16 +0530

    Full Darkn mode ended

--- app/(portfolio)/layout.tsx
+++ app/(portfolio)/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
 import { Atkinson_Hyperlegible } from "next/font/google";
 import "../globals.css";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { SanityLive } from "@/sanity/lib/live";
+
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import SidebarToggle from "@/components/sidebar-toggle";
 import Script from "next/script";
 import { FloatingDock } from "@/components/floating-dock";
+import { ModeToggle } from "@/components/dark-mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const atkins = Atkinson_Hyperlegible({
   subsets: ["latin"],
@@ -20,32 +24,40 @@ export const metadata: Metadata = {
   description: "An AI Portfolio generated using OpenAI's agentkit and chatkit",
 };
 
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const { userId } = await auth();
   const isAuthenticated = !!userId;
 
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body suppressHydrationWarning={true} className={`${atkins.className}`}>
-          <Script
-            src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
-            strategy="afterInteractive"
-          />
-          <SidebarProvider defaultOpen={false}>
-            <SidebarInset>{children}</SidebarInset>
-            {isAuthenticated && <AppSidebar side="right" />}
-<FloatingDock/>
-            <SidebarToggle />
-          </SidebarProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <html lang="en" suppressHydrationWarning>
+          <body className={atkins.className} suppressHydrationWarning>
 
-          <SanityLive />
-        </body>
-      </html>
+            <Script
+              src="https://cdn.platform.openai.com/deployments/chatkit/chatkit.js"
+              strategy="afterInteractive"
+            />
+
+            <SidebarProvider defaultOpen={false}>
+              <SidebarInset>{children}</SidebarInset>
+
+              {isAuthenticated && <AppSidebar side="right" />}
+
+              <FloatingDock />
+              <SidebarToggle />
+
+              <div className="fixed md:bottom-6 md:right-24 top-4 right-18 md:top-auto md:left-auto z-0">
+                <div className="w-10 h-10 md:w-12 md:h-12">
+                  <ModeToggle />
+                </div>
+              </div>
+            </SidebarProvider>
+
+            <SanityLive />
+          </body>
+        </html>
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
